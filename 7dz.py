a57df086b57af1753943bf14a9ab849ba075ca7b
Rodion06 <rodionbik06@gmail.com>
2026-03-23 23:56:48 +0300

    Update 7dz.py

--- 7dz.py
+++ 7dz.py
@@ -24,14 +24,21 @@ st.markdown("---")
 
 def generate_sample_data():
     seasonal_temperatures = {
-        "Moscow": {"winter": -10, "spring": 5, "summer": 18, "autumn": 8},
-        "Berlin": {"winter": 0, "spring": 10, "summer": 20, "autumn": 11},
-        "Beijing": {"winter": -2, "spring": 13, "summer": 27, "autumn": 16},
-        "Dubai": {"winter": 20, "spring": 30, "summer": 40, "autumn": 30},
-        "Cairo": {"winter": 15, "spring": 25, "summer": 35, "autumn": 25},
+        "New York": {"winter": 0, "spring": 10, "summer": 25, "autumn": 15},
         "London": {"winter": 5, "spring": 11, "summer": 18, "autumn": 12},
         "Paris": {"winter": 4, "spring": 12, "summer": 20, "autumn": 13},
-        "New York": {"winter": 0, "spring": 10, "summer": 25, "autumn": 15}
+        "Tokyo": {"winter": 6, "spring": 15, "summer": 27, "autumn": 18},
+        "Moscow": {"winter": -10, "spring": 5, "summer": 18, "autumn": 8},
+        "Sydney": {"winter": 12, "spring": 18, "summer": 25, "autumn": 20},
+        "Berlin": {"winter": 0, "spring": 10, "summer": 20, "autumn": 11},
+        "Beijing": {"winter": -2, "spring": 13, "summer": 27, "autumn": 16},
+        "Rio de Janeiro": {"winter": 20, "spring": 25, "summer": 30, "autumn": 25},
+        "Dubai": {"winter": 20, "spring": 30, "summer": 40, "autumn": 30},
+        "Los Angeles": {"winter": 15, "spring": 18, "summer": 25, "autumn": 20},
+        "Singapore": {"winter": 27, "spring": 28, "summer": 28, "autumn": 27},
+        "Mumbai": {"winter": 25, "spring": 30, "summer": 35, "autumn": 30},
+        "Cairo": {"winter": 15, "spring": 25, "summer": 35, "autumn": 25},
+        "Mexico City": {"winter": 12, "spring": 18, "summer": 20, "autumn": 15}
     }
     
     month_to_season = {
